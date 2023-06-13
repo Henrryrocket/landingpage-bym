75b6c069a3fff5404a03a729a0090df436c15f74
henrryrocket <Henrryrocket@gmail.com>
2023-06-13 03:33:50 -0300

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,14 +1,28 @@
-import { ContentfulContentSource } from '@stackbit/cms-contentful'
-import { defineStackbitConfig } from '@stackbit/types'
+import { GitContentSource } from '@stackbit/cms-git';
 
-export default defineStackbitConfig({
-  stackbitVersion: '~0.6.0',
-  contentSources: [
-    new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID!,
-      environment: process.env.CONTENTFUL_ENVIRONMENT!,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
-    }),
-  ],
-})
+import { Button } from './.stackbit/models/Button';
+import { Card } from './.stackbit/models/Card';
+import { Page } from './.stackbit/models/Page';
+import { CardGridSection } from './.stackbit/models/CardGridSection';
+import { FooterConfig } from './.stackbit/models/FooterConfig';
+import { HeroSection } from './.stackbit/models/HeroSection';
+import { SiteConfig } from './.stackbit/models/SiteConfig';
+
+export default sbConfig = {
+    stackbitVersion: '~0.6.0',
+    ssgName: 'nextjs',
+    nodeVersion: '16',
+    contentSources: [
+        new GitContentSource({
+            rootPath: __dirname,
+            contentDirs: ['content'],
+            models: [Button, Card, Page, CardGridSection, FooterConfig, HeroSection, SiteConfig],
+            assetsConfig: {
+                referenceType: 'static',
+                staticDir: 'public',
+                uploadDir: 'images',
+                publicPath: '/'
+            }
+        })
+    ]
+};
